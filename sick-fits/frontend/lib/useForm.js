d79bdc6e6183ce8382eaa64900a969682783fdaa
Aaron Rodriguez <aaroner@yahoo.com>
2022-02-09 23:35:03 -0600

    22/81

---
+++ sick-fits/frontend/lib/useForm.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // create a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
+
+  // {
+  //   name: 'aaron',
+  //   description: 'nice shoes',
+  //   price: 1000
+  // }
+
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+    // check if input is a number because inputs will return a string by default, change it if it is
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+    if (type === 'file') {
+      value[0] = e.target.files;
+    }
+    setInputs({
+      // copy the existing state
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    // create an object with the same keys, but update the values to be empty strings. Object.entries will return an array of arrays with the key [0], value pairs [1] respectively. then map over, destructure the key/values and return while updating the values. Then turn it back into an obj w/ Object.fromEntries
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  // return the things we want to surface from this custom hook
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
