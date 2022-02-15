ca615bfb90727e1e138a1f9697609f4ac647c965
Aaron Rodriguez <aaroner@yahoo.com>
2022-02-15 16:41:21 -0600

    27/83

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,8 +1,10 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -43,17 +45,24 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      // this allows us to tell the mutation to hit the network again and refetch data so you get an updated version and not the cached version (missing the added item)
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
   return (
     <Form
       onSubmit={async (e) => {
         e.preventDefault();
-        console.log(inputs);
         // submit the inputfields to the backend:
         // you can specify the variables used in the mutation EITHER when the mutation is defined(if you know what they are) or you can pass them when its called (below) as an object arg
-        await createProduct();
+        const res = await createProduct();
         clearForm();
+        // go to that products page
+        // we could have also used the 'data' obj here that is returned from useMutation
+        // can also pass query params if you need them
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`,
+        });
       }}
     >
       <DisplayError error={error} />
