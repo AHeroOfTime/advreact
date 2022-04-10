35b7b421a2f2ac33e5a1afa3f0ef6f29833ecbf4
Aaron Rodriguez <aaroner@yahoo.com>
2022-04-10 10:23:55 -0500

    70/83

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,7 +1,84 @@
 // at it's simplest, the access control returns a yes or no value depending on the users session
 
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ]),
+);
+
+// permissions check if someone meets a criteria - yes or no
+export const permissions = {
+  ...generatedPermissions,
+  isAwesome({ session }: ListAccessArgs): boolean {
+    return session?.data.name.includes('aaron');
+  },
+};
+
+// rule based function
+// rules can return a boolean - yes or no - or a filter which limits which products they can CRUD
+export const rules = {
+  canManageProducts({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    // 1. do they have the permission of canManageProducts
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+    // 2. if not, do they own this item
+    return { user: { id: session.itemId } };
+  },
+  canOrder({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    // 1. do they have the permission of canManageCart
+    if (permissions.canManageCart({ session })) {
+      return true;
+    }
+    // 2. if not, do they own this item
+    return { user: { id: session.itemId } };
+  },
+  canManageOrderItems({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    // 1. do they have the permission of canManageOrderItems
+    if (permissions.canManageCart({ session })) {
+      return true;
+    }
+    // 2. if not, do they own this item
+    return { order: { user: { id: session.itemId } } };
+  },
+  canReadProducts({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    if (permissions.canManageProducts({ session })) {
+      return true; // they can read everything
+    }
+    // they should only see available products based on status field
+    return { status: 'AVAILABLE' };
+  },
+  canManageUsers({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    // 1. do they have the permission of
+    if (permissions.canManageUsers({ session })) {
+      return true;
+    }
+    // otherwise they can only update themselves
+    return { id: session.itemId };
+  },
+};
--- sick-fits/backend/schemas/User.ts
+++ sick-fits/backend/schemas/User.ts
@@ -1,12 +1,23 @@
 import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
+import { permissions, rules } from '../access';
 
 export const User = list({
-  // access:
-  // ui:
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    // only people with the permission can delete themselves
+    delete: permissions.canManageUsers,
+  },
+  ui: {
+    // hide the ui from regular users
+    hideCreate: (args) => !permissions.canManageUsers(args),
+    hideDelete: (args) => !permissions.canManageUsers(args),
+  },
   fields: {
     name: text({ isRequired: true }),
-    email: text({ isRequired: true }),
+    email: text({ isRequired: true, isUnique: true }),
     password: password(),
     cart: relationship({
       ref: 'CartItem.user',
@@ -19,7 +30,14 @@ export const User = list({
     orders: relationship({ ref: 'Order.user', many: true }),
     role: relationship({
       ref: 'Role.assignedTo',
-      // TODO: add access control
+      access: {
+        create: permissions.canManageUsers,
+        update: permissions.canManageUsers,
+      },
+    }),
+    products: relationship({
+      ref: 'Product.user',
+      many: true,
     }),
   },
 });
