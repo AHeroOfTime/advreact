e295d94253c13145f2a80cd055988d8565642e8e
Aaron Rodriguez <aaroner@yahoo.com>
2022-04-11 13:53:23 -0500

    72/83

---
+++ sick-fits/frontend/__tests__/sample.test.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+  return parseInt(a + b);
+}
+
+describe('Sample test 101', () => {
+  it('works as expected', () => {
+    // we run our expect statements to see if the test will pass
+    expect(1).toEqual(1);
+    const age = 100;
+    expect(age).toEqual(100);
+  });
+  it('runs the add function properly', () => {
+    expect(add(1, 2)).toBeGreaterThanOrEqual(3);
+  });
+  it('can add strings of numbers together', () => {
+    expect(add('1', '2')).toBeGreaterThanOrEqual(3);
+  });
+});
